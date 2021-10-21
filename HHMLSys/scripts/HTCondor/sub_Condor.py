b711c51cd4954b56d80e5943fda614ec8e0216f9
Babar Ali <babar.ali@cern.ch>
2021-10-21 12:00:49 +0200

    small fix

--- HHMLSys/scripts/HTCondor/sub_Condor.py
+++ HHMLSys/scripts/HTCondor/sub_Condor.py
@@ -126,8 +126,16 @@ def createJobScript(files, outDir, jobKey):
 
     #Setup env.
     stext += 'export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase \n'
-    stext += 'source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh \n'
-    
+    stext += 'source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh \n\n'
+
+    stext += 'pwd=$PWD \n'
+    stext += 'echo pwd \n\n'
+    stext += 'lsetup asetup \n'
+    stext += 'cd /publicfs/atlas/atlasnew/higgs/HHML/Slim_sys_prod/build/ \n'
+    stext += 'asetup 21.2.182,AnalysisBase \n'
+    stext += 'source x86_64-*/setup.sh \n'
+    stext += 'cd pwd \n\n'
+
     #Check if file is copied or not. If not read the file directly. (wip: copy only the large files?)
     stext += 'echo Reading %s.root file directly'%files + '\n'
     stext += ' %s\n\n' %(' '.join(['runHHMLSys', jobOpts]))
