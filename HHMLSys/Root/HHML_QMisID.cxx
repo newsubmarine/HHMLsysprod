bf3ac3686c960d1af94c79bb0f96f3409dd3c5b0
Shuiting Xin <shxin@cern.ch>
2022-03-28 14:46:04 +0200

    local change

--- HHMLSys/Root/HHML_QMisID.cxx
+++ HHMLSys/Root/HHML_QMisID.cxx
@@ -3,12 +3,12 @@
 
 
 //--------------------------------------------------------------------------------------------
-void HHMLSys_EventSaver::getQMisIdRates(TH2D* rate_map, TH1D* proj_Eta, TH1D* proj_Pt,
-			    float eta, float pt,
+void HHMLSys_EventSaver::getQMisIdRates(TH3D* rate_map, TH1D* proj_Eta, TH1D* proj_Pt, TH1D* proj_R,
+			    float eta, float pt, float rg,
 			    float& r, float& r_up, float& r_dn )
 {
   Double_t this_low_edge(-999.0),this_up_edge(-999.0);
-  int xbin_nr(-1), ybin_nr(-1);
+  int xbin_nr(-1), ybin_nr(-1), zbin_nr(-1);
   
   // Loop over the projections, and keep track of the bin number where (x,y) is found
   
@@ -36,12 +36,25 @@ void HHMLSys_EventSaver::getQMisIdRates(TH2D* rate_map, TH1D* proj_Eta, TH1D* pr
 	}
     }
 
+    for(int zbin = 0; zbin < proj_Pt->GetNbinsZ()+1; ++zbin)
+    {
+      this_low_edge = proj_R->GetXaxis()->GetBinLowEdge(zbin);
+      this_up_edge  = proj_R->GetXaxis()->GetBinLowEdge(zbin+1);
+      
+      if( rg >= this_low_edge && rg < this_up_edge )
+	{
+	  zbin_nr = proj_R->GetBin(zbin);
+	  break;
+	}
+    }
+
+
   // Now get the NOMINAL rate via global bin number (x,y)
 
-  r = rate_map->GetBinContent( rate_map->GetBin( xbin_nr, ybin_nr ) );
+  r = rate_map->GetBinContent( rate_map->GetBin( xbin_nr, ybin_nr, zbin_nr) );
   
-  r_up = r + rate_map->GetBinErrorUp( rate_map->GetBin( xbin_nr, ybin_nr ) );
-  r_dn = r - rate_map->GetBinErrorUp( rate_map->GetBin( xbin_nr, ybin_nr ) );
+  r_up = r + rate_map->GetBinErrorUp( rate_map->GetBin( xbin_nr, ybin_nr, zbin_nr ) );
+  r_dn = r - rate_map->GetBinErrorUp( rate_map->GetBin( xbin_nr, ybin_nr, zbin_nr ) );
   r_dn = ( r_dn > 0.0 ) ? r_dn : 0.0;
 
 }
@@ -54,6 +67,7 @@ void HHMLSys_EventSaver::calcQMisIdWeights(float &weight_Nom, float &weight_Up, 
   if(isMuonPair()) return;
 
   float Pt0(-999.0), Eta0(999.0), Pt1(-999.0), Eta1(999.0);
+  int R0(-1), R1(-1);
 
   //r0 and r1 Up & Dn as well must be initialize with 0 and not with any other number.  
   float r0(0.0), r0_up(0.0), r0_dn(0.0), 
@@ -65,11 +79,58 @@ void HHMLSys_EventSaver::calcQMisIdWeights(float &weight_Nom, float &weight_Up, 
   Eta0 = std::fabs(ntup.lep_Eta_0);
   Eta1 = std::fabs(ntup.lep_Eta_1);
 
-  TH1D* proj_eta_T  = hist_QMisID_T->ProjectionX("proj_eta_T");
-  TH1D* proj_pt_T   = hist_QMisID_T->ProjectionY("proj_pt_T");
+  R0   = QMisID_Region_Lep0();
+  //TH1D* proj_eta_T  = hist_QMisID_T->ProjectionX("proj_eta_T");
+  //TH1D* proj_pt_T   = hist_QMisID_T->ProjectionY("proj_pt_T");
+  
+  // Tight or Anti-tight
+  bool  is_lep_0_tight(false), is_lep_1_tight(false);
+  
+  if(QMisID_Tight_Lep0){
     
-  getQMisIdRates(hist_QMisID_T, proj_eta_T, proj_pt_T, Eta0, Pt0, r0, r0_up, r0_dn);
-  getQMisIdRates(hist_QMisID_T, proj_eta_T, proj_pt_T, Eta1, Pt1, r1, r1_up, r1_dn);
+    is_lep_0_tight = true;
+    TH1D* proj_eta  = hist_QMisID_T->ProjectionX("proj_eta");
+    TH1D* proj_pt   = hist_QMisID_T->ProjectionY("proj_pt" );
+    TH1D* proj_rg   = hist_QMisID_T->ProjectionZ("proj_rg" );
+    getQMisIdRates(hist_QMisID_T, proj_eta, proj_pt, proj_rg, Eta0, Pt0, R0, r0, r0_up, r0_dn);
+
+  }
+  else{
+
+    is_lep_0_tight = false;
+    TH1D* proj_eta  = hist_QMisID_AT->ProjectionX("proj_eta");
+    TH1D* proj_pt   = hist_QMisID_AT->ProjectionY("proj_pt" );
+    TH1D* proj_rg   = hist_QMisID_AT->ProjectionZ("proj_rg" );
+    getQMisIdRates(hist_QMisID_AT, proj_eta, proj_pt, proj_rg, Eta0, Pt0, R0, r0, r0_up, r0_dn);
+
+  }
+    
+  if(QMisID_Tight_Lep1){
+    
+    is_lep_1_tight = true;
+    TH1D* proj_eta  = hist_QMisID_T->ProjectionX("proj_eta");
+    TH1D* proj_pt   = hist_QMisID_T->ProjectionY("proj_pt" );
+    TH1D* proj_rg   = hist_QMisID_T->ProjectionZ("proj_rg" );
+    getQMisIdRates(hist_QMisID_T, proj_eta, proj_pt, proj_rg, Eta1, Pt1, R1, r1, r1_up, r1_dn);
+
+  }
+  else{
+
+    is_lep_1_tight = false;
+    TH1D* proj_eta  = hist_QMisID_AT->ProjectionX("proj_eta");
+    TH1D* proj_pt   = hist_QMisID_AT->ProjectionY("proj_pt" );
+    TH1D* proj_rg   = hist_QMisID_AT->ProjectionZ("proj_rg" );
+
+    getQMisIdRates(hist_QMisID_AT, proj_eta, proj_pt, proj_rg, Eta1, Pt1, R1, r1, r1_up, r1_dn);
+
+  }
+  
+
+
+
+
+  //getQMisIdRates(hist_QMisID_T, proj_eta_T, proj_pt_T, Eta0, Pt0, r0, r0_up, r0_dn);
+  //getQMisIdRates(hist_QMisID_T, proj_eta_T, proj_pt_T, Eta1, Pt1, r1, r1_up, r1_dn);
 
   if (abs(ntup.lep_ID_0)==13) {
     r0=0;
@@ -134,10 +195,10 @@ void HHMLSys_EventSaver::getQMisIDWeights() {
 
 
   if( !m_isData ) return;
-  if( !ntup.passEventCleaning ) return;
-  if( !(SLTrigCuts() || DLTrigCuts()) ) return;
-  if( !ntup.dilep_type ) return;
-  if( !LepTrigMatch("SLTorDLT_Tight") ) return;
+  //if( !ntup.passEventCleaning ) return;
+  //if( !(SLTrigCuts() || DLTrigCuts()) ) return;
+  //if( !ntup.dilep_type ) return;
+  //if( !LepTrigMatch("SLTorDLT_Tight") ) return;
   if( !isOSPair() ) return;        //OS pair
   if( isMuonPair() ) return;
 
